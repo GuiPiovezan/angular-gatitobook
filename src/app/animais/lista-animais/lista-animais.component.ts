8cd63d3a5cce46cac1c31a11438b01e1ad9bce05
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2022-04-20 08:56:41 -0300

    Criando o componente de listagem de animais e implementando a guarda de rotas para controlar o fluxo de navegação do usuário

--- src/app/animais/lista-animais/lista-animais.component.ts
+++ src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,15 +1,35 @@
+import { Observable, switchMap } from 'rxjs';
+import { AnimaisService } from './../animais.service';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Animais } from './../animais';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista-animais',
   templateUrl: './lista-animais.component.html',
-  styleUrls: ['./lista-animais.component.css']
+  styleUrls: ['./lista-animais.component.css'],
 })
 export class ListaAnimaisComponent implements OnInit {
+  animais$!: Observable<Animais>;
 
-  constructor() { }
+  constructor(
+    private usuarioService: UsuarioService,
+    private animaisService: AnimaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    // this.usuarioService.retornarUsuario().subscribe((usuario) => {
+    //   const userName = usuario.name ?? '';
+    //   this.animaisService.listaDoUsuario(userName).subscribe((animais) => {
+    //     this.animais = animais;
+    //   });
+    // });  // subscribe hell (não é muito utilizado uma cadeia de subscribes)
 
+    this.animais$ = this.usuarioService.retornarUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      })
+    );
+  }
 }
